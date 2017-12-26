3a9d03e3bbc725ebe4e5df4572dea75a156f87cb
lburgosr1 <34818919+lburgosr1@users.noreply.github.com>
2017-12-26 19:37:32 -0400

    Update app.js

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ var bodyParser = require('body-parser');
 var app = express();
 
 // view engine setup
-app.set('port', process.env.PORT || 3000);
+//app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
@@ -69,5 +69,9 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
-module.exports = {app: app, server: server};
+if(!! module.parent){
+	module.exports = app;//: app, server: server};
+}else{
+	app.listen(3000);
+}
+	
